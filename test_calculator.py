69eedf05031fb673fce63653739ffc146e083b69
Shakir31 <s10269080@connect.np.edu.sg>
2025-11-22 08:27:03 +0800

    added power function and test

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,5 @@
 import pytest
-from calculator import add, subtract, multiply, divide
+from calculator import add, subtract, multiply, divide, power
 
 def test_add():
     assert add(1,1)==2
@@ -24,4 +24,8 @@ def test_divide():
 def test_divide_by_zero():
     with pytest.raises(ValueError):
         divide(10,0)
-    
+
+def test_power():
+    assert power(2,3)==8
+    assert power(2,0)==1
+    assert power(0,2)==0
