114d86d42d3e966bff27264c3542edcc7e4d0b83
Shakir31 <s10269080@connect.np.edu.sg>
2025-11-22 08:08:44 +0800

    Initial commit: Add calculator and tests

---
+++ test_calculator.py
@@ -0,0 +1,27 @@
+import pytest
+from calculator import add, subtract, multiply, divide
+
+def test_add():
+    assert add(1,1)==2
+    assert add(-1, 1)==0
+    assert add(0,0)==0
+
+def test_subtract():
+    assert subtract(2,1)==1
+    assert subtract(0,1)==-1
+    assert subtract(0,0)==0
+
+def test_multiply():
+    assert multiply(2,2)==4
+    assert multiply(1,0)==0
+    assert multiply(-5,2)==-10
+
+def test_divide():
+    assert divide(4,2)==2
+    assert divide(2,2)==1
+    assert divide(-10,2)==-5
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(10,0)
+    
